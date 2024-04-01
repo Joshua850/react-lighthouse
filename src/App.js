963376522a001d62946fb0a7d6ea84d9bb55f235
Joshua Cox <joshua.cox@davra.com>
2024-04-01 19:26:37 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MovingCircle from "./movingCircle";
 
 function App() {
   return (
     <div className="App">
+      <div className="sky"></div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="pole"></div>
+        <div className="flag"></div>
+        {<MovingCircle />}
+        <div className="cylinder"></div>
+        <div className="cylinder2"></div>
+        <div className="cylinder"></div>
+        <div className="cylinder2"></div>
+
+        <div className="bottom"></div>
+        <div class="grass"></div>
       </header>
     </div>
   );
---
+++ src/movingCircle.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MovingCircle.css"; // Import CSS file for styling
+
+const MovingCircle = () => {
+  return (
+    <div className="container">
+        <div className="circle" />
+
+      </div>   
+      
+  );
+};
+
+export default MovingCircle;
